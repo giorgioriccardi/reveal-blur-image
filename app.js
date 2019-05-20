f3a3a2efaf161af599beb587737f4c56c6bb9446
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-05-20 09:44:20 -0700

    first draft es6

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 /**  code by ssws ( https://ssws.com ) **/
-$('.pic').mousemove(function (event) {
+// $('.pic').mousemove(function (event) {
+//     event.preventDefault();
+//     var upX = event.clientX;
+//     var upY = event.clientY;
+//     var mask = $('#mask1 circle')[0];
+//     mask.setAttribute("cy", (upY - 5) + 'px');
+//     mask.setAttribute("cx", (upX) + 'px');
+// });
+
+// ES6
+
+document.querySelector(".pic").addEventListener("mousemove", function (event) {
     event.preventDefault();
-    var upX = event.clientX;
-    var upY = event.clientY;
-    var mask = $('#mask1 circle')[0];
+    let upX = event.clientX;
+    let upY = event.clientY;
+    let mask = document.getElementById("#mask1");
     mask.setAttribute("cy", (upY - 5) + 'px');
     mask.setAttribute("cx", (upX) + 'px');
-});
+}, false);
