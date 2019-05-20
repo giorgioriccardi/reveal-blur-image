b94fe59bde69bbc294b369d8193c6354aa57bd09
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-05-20 09:15:49 -0700

    create initial jquery codebase

---
+++ app.js
@@ -0,0 +1,9 @@
+/**  code by webdevtrick ( https://webdevtrick.com ) **/
+$('.pic').mousemove(function (event) {
+    event.preventDefault();
+    var upX = event.clientX;
+    var upY = event.clientY;
+    var mask = $('#mask1 circle')[0];
+    mask.setAttribute("cy", (upY - 5) + 'px');
+    mask.setAttribute("cx", (upX) + 'px');
+});
