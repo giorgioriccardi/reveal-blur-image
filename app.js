4ed8873029b51c8c4faf9ab761efc94d5e83ebf4
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-05-20 10:01:20 -0700

    remove references to jquery

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 /**  code by ssws ( https://ssws.com ) **/
-// $('.pic').mousemove(function (event) {
-//     event.preventDefault();
-//     var upX = event.clientX;
-//     var upY = event.clientY;
-//     var mask = $('#mask1 circle')[0];
-//     mask.setAttribute("cy", (upY - 5) + 'px');
-//     mask.setAttribute("cx", (upX) + 'px');
-// });
 
 // ES6
 
@@ -19,3 +11,14 @@ document.querySelector(".pic").addEventListener("mousemove", function (event) {
     circle.setAttribute("cy", (upY - 5) + 'px');
     circle.setAttribute("cx", (upX) + 'px');
 });
+
+// Original jQuery
+
+// $('.pic').mousemove(function (event) {
+//     event.preventDefault();
+//     var upX = event.clientX;
+//     var upY = event.clientY;
+//     var mask = $('#mask1 circle')[0];
+//     mask.setAttribute("cy", (upY - 5) + 'px');
+//     mask.setAttribute("cx", (upX) + 'px');
+// });
